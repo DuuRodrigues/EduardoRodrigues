955e734f0f9b99421a4f3d30332ca946fbcdb799
Eduardo Rodrigues <101715637+DuuRodrigues@users.noreply.github.com>
2022-07-07 13:43:02 -0300

    alteração de Cores

---
+++ index.js
@@ -0,0 +1,15 @@
+function darkblue() {
+    return (
+      document.getElementById('main').className = ('dark-blue') 
+    )
+  }
+  
+  function pink() {
+    return (
+      document.getElementById('main').className = ('pink')
+    )
+  }
+  
+  function darkpink() {
+    document.getElementById('main').className = ('dark-pink')
+  }
