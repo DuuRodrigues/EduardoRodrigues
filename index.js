33fe511a7070c119ca2ed54471d7e4b3d2555b7c
Eduardo Rodrigues <101715637+DuuRodrigues@users.noreply.github.com>
2022-07-08 14:07:14 -0300

    Random

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function darkblue() {
-    return (
-      document.getElementById('main').className = ('dark-blue') 
-    )
-  }
-  
-  function pink() {
-    return (
-      document.getElementById('main').className = ('pink')
-    )
-  }
-  
-  function darkpink() {
-    document.getElementById('main').className = ('dark-pink')
-  }
+  return document.getElementById('main').className = ('dark-blue')
+}
+
+function pink() {
+  return (
+    document.getElementById('main').className = ('pink')
+  )
+}
+
+function darkpink() {
+  document.getElementById('main').className = ('dark-pink')
+}
+
+function colorTime() {
+  let a = ['dark-blue', 'dark-pink', 'pink', '']
+  let randomItem = a[Math.floor(Math.random() * a.length)];
+  document.getElementById('main').className = randomItem
+}
